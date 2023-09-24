a18416bd4b9e0a5fd36fc0ca6792d6ca5464a1d4
cleytonmuto <cleytonmuto@gmail.com>
2023-09-24 18:16:35 -0300

    fix: return to working version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1'
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <Component {...pageProps} />
     </>
   );
