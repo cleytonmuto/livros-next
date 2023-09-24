b8bd4409099e08762b49ee39a81a0f908b041a7f
cleytonmuto <cleytonmuto@gmail.com>
2023-09-24 18:27:41 -0300

    fix: metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import type { AppProps } from 'next/app';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    name: 'viewport',
-    content: 'width=device-width, initial-scale=1'
-};
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
